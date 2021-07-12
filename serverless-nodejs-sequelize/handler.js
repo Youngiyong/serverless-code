8fca7281f8514b1dbd4562ecd868882bc6432fb7
giyong <youn9354@naver.com>
2021-07-13 00:27:14 +0900

    rds db 연결 작업중

--- serverless-nodejs-sequelize/handler.js
+++ serverless-nodejs-sequelize/handler.js
@@ -1,21 +1,34 @@
 'use strict';
-
+var mysql = require("mysql"); 
 
 module.exports.hello = async (event) => {
-  await connectToDatabase()
-  console.log('connection success')
+  var connection = mysql.createConnection
+  ({
+  host: process.env.RDS_HOSTNAME,
+  user: process.env.RDS_RDS_USERNAME,
+  password: process.env.RDS_PASSWORD,
+  database: process.env.RDS_DATABASE
+  })
+  console.log("createConnection 성공")
+  
+  await connection.connect();
 
-  return {
-    statusCode: 200,
-    body: JSON.stringify(
-      {
-        message: 'Go Serverless v1.0! Your function executed successfully!',
-        input: event,
-      },
-      null,
-      2
-    ),
-  };
+
+  await connection.query("SELECT * FROM tags", function(err, rows, fields) {
+    console.log(err,rows,fields); 
+  });
+
+  // return {
+  //   statusCode: 200,
+  //   body: JSON.stringify(
+  //     {
+  //       message: 'Go Serverless v1.0! Your function executed successfully!',
+  //       input: event,
+  //     },
+  //     null,
+  //     2
+  //   ),
+  // };
 
   // Use this code if you don't use the http event with the LAMBDA-PROXY integration
   // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
